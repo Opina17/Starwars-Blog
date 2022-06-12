1a726caba29a7defd31677aaf46f60ecd5f5388f
Opina17 <omarpp2596@gmail.com>
2022-06-12 18:39:59 -0400

    first commit

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,8 +4,8 @@ export const Footer = () => {
 	return(
 		<footer className="footer">
 		<p className="m-0">
-			Made with <i className="fa fa-heart text-danger" /> by Jose Velasquez a <br/>
-			<a target="_blank" href="http://www.4geeksacademy.com"> 4Geeks Academy</a> student
+			Made with <i className="fa fa-heart text-danger" /> by  <br/>
+			<a target="_blank" href="http://www.4geeksacademy.com"> 4Geeks Academy</a>
 		</p>
 	</footer>
 	);
